3fbe3e46bdb586c30a3919e0d6c80f3d9ecd2d18
Ritwiz <madangopalsingh2803@gmail.com>
2025-12-24 01:37:56 +0530

    displaying block after some delay using lifecycle hooks

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,20 +1,36 @@
 <template>
-  <div class="block">
-    click me
-  </div>
+  <div class="block" v-if="showBlock">click me</div>
 </template>
 
 <script>
 export default {
-    props: ['delay'],
-}
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    console.log(`Block mounted`);
+    setTimeout(() => {
+      this.showBlock = true;
+      console.log("Block is now visible after delay of " + this.delay + "ms");
+    }, this.delay);
+  },
+  updated() {
+    console.log("Block updated");
+  },
+  unmounted() {
+    console.log("Block unmounted");
+  },
+};
 </script>
 
 <style>
 .block {
   width: 400px;
   border-radius: 20px;
-  background:  #0faf87;
+  background: #0faf87;
   color: white;
   text-align: center;
   padding: 100px 0;
